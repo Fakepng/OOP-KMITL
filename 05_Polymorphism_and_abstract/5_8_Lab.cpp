b9463e3fb91c84fd911d822cedb95e3416ac3b55
Fakepng <krit0kasemtewin@gmail.com>
2024-01-08 10:59:06 +0700

    Week 5

--- 05_Polymorphism_and_abstract/5_8_Lab.cpp
+++ 05_Polymorphism_and_abstract/5_8_Lab.cpp
@@ -8,11 +8,51 @@ public :
     virtual int subtract(int x,int y)  = 0;
     virtual int multiply(int x,int y)  = 0;
 
-    virtual int sum(int x1,int x2,int x3,int x4,int x5,int x6,int x7,int x8,int x9,int x10) = 0;
+     virtual int sum(int x1,int x2,int x3,int x4,int x5,int x6,int x7,int x8,int x9,int x10) = 0;
 
     // special point
-    //virtual int avg(int x1,int x2,int x3,int x4,int x5) = 0;
-    
+    virtual int avg(int x1,int x2,int x3,int x4,int x5) = 0;
+
+};
+
+class int_calculator : public prototype_calculator{
+public :
+    int add(int x,int y){
+        return x + y;
+    }
+
+    int subtract(int x,int y){
+        return x - y;
+    }
+
+    int multiply(int x,int y){
+        return x * y;
+    }
+
+    int sum(int x1 = 0,int x2 = 0,int x3 = 0,int x4 = 0,int x5 = 0,int x6 = 0,int x7 = 0,int x8 = 0,int x9 = 0,int x10 = 0){
+        return x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10;
+    }
+
+    // special points
+    int avg(int x1,int x2,int x3,int x4,int x5){
+       return (x1 + x2 + x3 + x4 + x5) / 5;
+    }
+
+    int avg(int x1,int x2,int x3,int x4){
+        return (x1 + x2 + x3 + x4) / 4;
+    }
+
+    int avg(int x1,int x2,int x3){
+        return (x1 + x2 + x3) / 3;
+    }
+
+    int avg(int x1,int x2){
+        return (x1 + x2) / 2;
+    }
+
+    int avg(int x1){
+        return x1;
+    }
 };
 
 int main(){
@@ -26,8 +66,8 @@ int main(){
     cout << cal.sum(1,2,3) << "," << cal.sum(1,2) << "," << cal.sum(1) << endl;
 
     // special point
-    //cout << "avg" << endl;
-    //cout << cal.avg(1,2,3,4,5) << "," << cal.avg(1,2,3,4) << "," << cal.avg(1,2,3) << "," << cal.avg(1,2) << "," << cal.avg(1) << endl;
+    cout << "avg" << endl;
+    cout << cal.avg(1,2,3,4,5) << "," << cal.avg(1,2,3,4) << "," << cal.avg(1,2,3) << "," << cal.avg(1,2) << "," << cal.avg(1) << endl;
 
     return 0;
 }
--- 05_Polymorphism_and_abstract/5_9_Lab.cpp
+++ 05_Polymorphism_and_abstract/5_9_Lab.cpp
@@ -11,8 +11,49 @@ public :
     virtual double sum(double x1,double x2,double x3,double x4,double x5,double x6,double x7,double x8,double x9,double x10) = 0;
 
     // special point
-    //virtual double avg(double x1,double x2,double x3,double x4,double x5) = 0;
-    
+    virtual double avg(double x1,double x2,double x3,double x4,double x5) = 0;
+
+};
+
+class double_calculator : public prototype_calculator{
+public :
+    double add(double x,double y){
+        return x + y;
+    }
+
+    double subtract(double x,double y){
+        return x - y;
+    }
+
+    double multiply(double x,double y){
+        return x * y;
+    }
+
+    double sum(double x1 = 0,double x2 = 0,double x3 = 0,double x4 = 0,double x5 = 0,double x6 = 0,double x7 = 0,double x8 = 0,double x9 = 0,double x10 = 0){
+        return x1 + x2 + x3 + x4 + x5 + x6 + x7 + x8 + x9 + x10;
+    }
+
+    // special points
+    double avg(double x1,double x2,double x3,double x4,double x5){
+       return (x1 + x2 + x3 + x4 + x5) / 5;
+    }
+
+    double avg(double x1,double x2,double x3,double x4){
+        return (x1 + x2 + x3 + x4) / 4;
+    }
+
+    double avg(double x1,double x2,double x3){
+        return (x1 + x2 + x3) / 3;
+    }
+
+    double avg(double x1,double x2){
+        return (x1 + x2) / 2;
+    }
+
+    double avg(double x1){
+        return x1;
+    }
+
 };
 
 int main(){
@@ -26,8 +67,8 @@ int main(){
     cout << cal.sum(1,2,3) << "," << cal.sum(1,2) << "," << cal.sum(1) << endl;
 
     // special point
-    // << "avg" << endl;
-    //cout << cal.avg(1,2,3,4,5) << "," << cal.avg(1,2,3,4) << "," << cal.avg(1,2,3) << "," << cal.avg(1,2) << "," << cal.avg(1) << endl;
+    cout << "avg" << endl;
+    cout << cal.avg(1,2,3,4,5) << "," << cal.avg(1,2,3,4) << "," << cal.avg(1,2,3) << "," << cal.avg(1,2) << "," << cal.avg(1) << endl;
 
     return 0;
 }
